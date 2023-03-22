3d298d9802b0b3b712e2e38e73eb0f1a5e6f8354
IanSylvester <sylvester16gmail.com>
2023-03-22 14:21:45 -0400

    fixed mobile toggle show not show

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -46,6 +46,7 @@ const Navbar = () => {
             </li>
           ))}
         </ul>
+
         <div className="sm:hidden flex flex-1 justify-end items-center">
           <img
             src={toggle ? close : menu}
@@ -56,7 +57,7 @@ const Navbar = () => {
           <div
             className={`${
               !toggle ? "hidden" : "flex"
-            }p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w[140px] z-10 rounded-xl`}
+            } p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}
           >
             <ul className="list-none flex justify-end items-start flex-col gap-4">
               {navLinks.map((Link) => (
