132ef6085c7dda56b14905be64e29a4822d1e6db
IanSylvester <sylvester16gmail.com>
2023-03-22 15:54:17 -0400

    prettier

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,16 @@
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from "react-router-dom";
 
-import {About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from './components';
+import {
+  About,
+  Contact,
+  Experience,
+  Feedbacks,
+  Hero,
+  Navbar,
+  Tech,
+  Works,
+  StarsCanvas,
+} from "./components";
 
 const app = () => {
   return (
@@ -15,13 +25,13 @@ const app = () => {
         <Tech />
         <Works />
         <Feedbacks />
-        <div className='relative z-0'>
+        <div className="relative z-0">
           <Contact />
           <StarsCanvas />
-        </div>  
+        </div>
       </div>
     </BrowserRouter>
-  )
-}
+  );
+};
 
-export default app
+export default app;
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -18,10 +18,12 @@ const Hero = () => {
             Hi, I'm <span className="text-[#915eff]">Ian Sylvester </span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I am an aspiring software developer <br className="sm:block hidden" /> currently focusing on 3D visuals, <br className="sm:block hidden" /> user interfaces and web applications.
+            I am an aspiring software developer{" "}
+            <br className="sm:block hidden" /> currently focusing on 3D visuals,{" "}
+            <br className="sm:block hidden" /> user interfaces and web
+            applications.
           </p>
         </div>
-        
       </div>
       <ComputersCanvas />
     </section>
