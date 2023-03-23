18f0f08b05e762c1fc626e1bb3df96e2e701bf8e
IanSylvester <sylvester16gmail.com>
2023-03-23 11:32:39 -0400

    tweeked shading and float formats

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -14,20 +14,23 @@ const Ball = (props) => {
   const [decal] = useTexture([props.imgUrl]);
 
   return (
-    <Float speed={0.75} rotationIntensity={1} floatIntensity={2}>
+    <Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
       <ambientLight intensity={0.25} />
       <directionalLight position={[0, 0, 0.05]} />
       <mesh castShadow receiveShadow scale={2.75}>
         <icosahedronGeometry args={[1, 1]} />
-        <meshStandardMaterial 
-        color="#fff8eb"
-        polygonOffset
-        polygonOffsetFactor={-5}/>
+        <meshStandardMaterial
+          color="#fff8eb"
+          polygonOffset
+          polygonOffsetFactor={-5}
+          flatShading
+        />
         <Decal
-        position={[0, 0, 1]}
-        rotation={[2 * Math.PI, 0, 6.25]}
-        flatShading
-        map={decal} />
+          position={[0, 0, 1]}
+          rotation={[2 * Math.PI, 0, 6.25]}
+          flatShading
+          map={decal}
+        />
       </mesh>
     </Float>
   );
