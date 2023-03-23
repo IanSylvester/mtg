ec3a511d51c01d8cbd7d1d4ab2437d13765c4abc
IanSylvester <sylvester16gmail.com>
2023-03-23 15:51:46 -0400

    created starCanvas and animation

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -6,13 +6,14 @@ import CanvasLoader from "../Loader";
 
 const Earth = () => {
   const earth = useGLTF("./planet/scene.gltf");
-  
+    
   return (
-  <primitive object={earth.scene} 
-    scale={2.5}
-    position-y={0}
-    rotation-y={0}
-  />
+    <primitive 
+      object={earth.scene} 
+      scale={2.5}
+      position-y={0}
+      rotation-y={0}
+    />
   );
 };
 
